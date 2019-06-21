9b86c30e9cd67f9fc3f961b44c659bd318bfd3d5
Fernando Silva <fehshuffle@gmail.com>
2019-06-21 11:58:57 -0300

    bitcoin retatorado

--- js/fetch-bitcoin.js
+++ js/fetch-bitcoin.js
@@ -1,8 +1,8 @@
-export default function initFetchBitcoin() {
-  fetch('https://blockchain.info/ticker')
+export default function fetchBitcoin(url, target) {
+  fetch(url)
     .then(btc => btc.json())
     .then(bitcoin => {
-      const btcpreco = document.querySelector('.btc-preco');
+      const btcpreco = document.querySelector(target);
       btcpreco.innerText = 'R$ ' + (bitcoin.BRL.buy).toFixed(0);
     }).catch(err => {
       console.log(err);
